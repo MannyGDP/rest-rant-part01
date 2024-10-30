0d69ac1d441286b03a656ad1343522fcda242b1d
Manny Delgado <mannygdp@gmail.com>
2024-10-29 21:39:31 -0700

    added part 4-4

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.use('/places', require('./controllers/places'));
 
--- views/Default.jsx
+++ views/Default.jsx
@@ -6,6 +6,7 @@ function Def(html) {
     <head>
         <title>Rest Rant</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/css/style.css"/>
     </head>
     <body>
         { html.children }
