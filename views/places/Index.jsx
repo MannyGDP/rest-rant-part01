3f0de51e7e3f141d120369d0e15c21871344b490
Manny Delgado <mannygdp@gmail.com>
2024-11-04 22:45:56 -0800

    added a button to the places page

--- views/places/Index.jsx
+++ views/places/Index.jsx
@@ -5,13 +5,16 @@ function Index({ places }) {
     return (
   <Def>
     <main>
-        <h1>PLACES INDEX PAGE</h1>
+        <h1>PLACES</h1>
         <div className='row'>
         { places.map(place => (
           <div className='col-sm-6'>
+                <a href="/">
+                <button className='btn-primary'>Back to Home</button>
+                </a>
                 <h2>{ place.name }</h2>
-                <p className='text-center'>{ place.cuisines }</p>
                 <img src={ place.pic } alt={ place.name } />
+                <p className='text-center'>{ place.cuisines }</p>
                 <p className='text-center'>
                   Located in { place.city }, { places.state }
                 </p>
